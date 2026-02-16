de16ab8398c4a05909af3ddfb2e4453d7d4ebbd9
Pintikoff <petrovskiy.lev@gmail.com>
2026-02-16 02:01:06 +0100

    Add a comment with functions list and its location

--- main.c
+++ main.c
@@ -9,13 +9,31 @@
 #include "bmp_other.h"
 
 /*
---------Function list and it's location---------
-bmp_edit.c:
+!--------Function list and it's location---------
+? bmp_edit.c:
+    * Pixel** allocate2DPixelArray(int rows, int cols);
 
-bmp_read.c:
+    * void mirrorX(InfoHeader *infoHeader, Pixel** pixelMap);
+    * void mirrorY(InfoHeader *infoHeader, Pixel** pixelMap);
 
-bmp_other.c:
+    * void rotate90(InfoHeader *infoHeader, Pixel*** pixelMap);
+    * void rotate180(InfoHeader *infoHeader, Pixel** pixelMap);
+    * void rotate270(InfoHeader *infoHeader, Pixel ***pixelMap);
 
+    * void snapImage(InfoHeader *infoHeader, Pixel ***pixelMap, int startX, int startY, int endX, int endY);
+    * void addFrame(InfoHeader *infoHeader, Pixel ***pixelMap, uint32_t frameWidth, uint8_t frameR, uint8_t frameG, uint8_t frameB);
+    * void changeTint(InfoHeader* infoHeader, Pixel **pixelMap, char tintColorString, uint8_t tintValue);
+    * void zoom(InfoHeader* infoHeader, Pixel ***pixelMap, uint32_t zoomValue);
+? bmp_read.c:
+    * uint8_t* readHeader(char* link, Header* header,  InfoHeader* infoHeader);
+    * Pixel** readPixels(Header *header, InfoHeader *infoHeader, uint8_t *buffer);
+? bmp_other.c:
+    * void cleanMem(Header* header, InfoHeader* infoHeader, uint8_t* buffer, Pixel** pixelMap);
+    * void writeNewFile(Header *header, InfoHeader *infoHeader, Pixel** pixelMap);
+    * void printHelp();
+    * void outPutStructTemp(Header* header, InfoHeader* infoHeader);
+    * void outputPixels(InfoHeader *infoHeader, Pixel** pixelMap);
+    * int checkFileType(Header* header, InfoHeader* infoHeader);
 */
 
 int main(int argc, char *argv[]) {    
